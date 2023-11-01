be15efa661635246e7454722c72bc60d233a6d18
Lærke Nielsen <laenie01@easv365.dk>
2023-11-01 22:20:25 +0100

    displays a paragraph with the converter history when calculating a measurement. updated the text that tells the user what is printed out

--- BDE_exam/src/classes/ounce.ts
+++ BDE_exam/src/classes/ounce.ts
@@ -1,18 +1,20 @@
 import { HasFormatter } from '../modules/HasFormatter.js'
 
-// class for Ounce
+// interface with classes for Ounce
 // 'implements HasFormatter' tells it to follow the structure of HasFormatter
 export class Ounce implements HasFormatter{
   
     // tell system what to expect with constructor
     constructor (
-        public convertFrom: string,
-        public amount: number,
-        public convertTo: string,
+        private convertFrom: string, // make private so it isn't accessable to change the value
+        public amount: number, // make public so it is accessable to change the value
+        private convertTo: string, // make private so it isn't accessable to change the value
+        private results: number, // make private so it isn't accessable to change the value
     ) {}
   
+    // prints out what was the result of the conversion
     format() {
-      return `Converting ${this.amount} ${this.convertFrom}(s) to ${this.convertTo}(s)`;
+      return `${this.results} ${this.convertTo}(s) = ${this.amount} ${this.convertFrom}(s)`;
     }
   }
 
--- BDE_exam/src/main.ts
+++ BDE_exam/src/main.ts
@@ -39,6 +39,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
       <ul class="result-list">
         <p class="result"></p>
+        <p class="resultHistory"></p>
       </ul>
 
     </form>
@@ -52,8 +53,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 let docOne: HasFormatter;
 let docTwo: HasFormatter;
 
-docOne = new Pound("Pound", 2, "Gram");
-docTwo = new Ounce("Ounce", 5, "Kilogram");
+docOne = new Pound("Pound", 2, "Gram", 5);
+docTwo = new Ounce("Ounce", 5, "Kilogram", 3);
 
 
 // can be added as long as they have the HasFormatter interface
@@ -65,8 +66,8 @@ console.log(docs);
 
 
 // Measure class
-const resultOne = new Pound("Pound", 1, "Gram");
-const resultTwo = new Pound("Ounce", 3, "Gram");
+const resultOne = new Pound("Pound", 1, "Gram", 6);
+const resultTwo = new Pound("Ounce", 3, "Gram", 5);
 
 
 // create an array to store the results under one 
@@ -126,7 +127,7 @@ const list = new listResults(ul);
 
 
 
-// prints out the result of the convert
+// prints out the result of the convert and keeps the result in the list
 const calculateConvert = () => {
 
   // Store the static number of weights
@@ -163,389 +164,32 @@ const calculateConvert = () => {
 formAmerican.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  let doc: HasFormatter;
-  if (convertFrom.value === 'pound') {
-    doc = new Pound(convertFrom.value, amount.valueAsNumber, convertTo.value)
-  } else {
-    doc = new Ounce(convertFrom.value, amount.valueAsNumber, convertTo.value)
-  }
-
-  // write the list out
-  list.render(doc, convertFrom.value, 'end')
-
 
   const results = calculateConvert();
 
 
-  // Display the result in your HTML
+  // Display the result with the converted amount and the value it was converted to in HTML
   const resultElement = document.querySelector('.result') as HTMLElement;
-  resultElement.innerHTML = `Result: ${results}`;
+  resultElement.innerHTML = `Result: ${results} ${convertTo.value}(s)`;
 
+  // Display a paragraph with history result once a result has been calculated
+  const resultHistoryElement = document.querySelector('.resultHistory') as HTMLElement;
+  resultHistoryElement.innerHTML = `Result history`;
+
+
+  // display the history of the results
+  let doc: HasFormatter;
+  if (convertFrom.value === 'pound') {
+    doc = new Pound(convertFrom.value, amount.valueAsNumber, convertTo.value, results)
+  } else {
+    doc = new Ounce(convertFrom.value, amount.valueAsNumber, convertTo.value, results)
+  }
+
+  
+  // write the list out
+  list.render(doc, convertFrom.value, 'end')
+
+
+  
   console.log(doc);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // links to european to american converter
-// const formEuropean = document.querySelector('.converter-european-to-american') as HTMLFormElement;
-// console.log(formEuropean.children);
-
-
-
-// const anchor = document.querySelector('a');
-
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-
-
-
-
-
-// // type  AddNumbersFunction = (a: number, b: number) => number;
-// // const addNumbers: AddNumbersFunction = (a, b) => a + b;
-// // const result: number = addNumbers(1, 2);
-// // console.log(result);
-
-
-
-
-// // // Interface about american lengths
-// // type americanLengths = {
-// //   inch: number,
-// //   foot: number,
-// //   yard: number,
-// //   mile: number,
-// // }
-
-
-
-
-
-// // interface AmericanLengths {
-// //   inch: number,
-// //   foot: number,
-// //   yard: number,
-// //   mile: number,
-// // }
-
-// // const americanLengths: AmericanLengths = {
-// //   inch: 2.54,
-// //   foot: 12,
-// //   yard: 36,
-// //   mile: 63360,
-// // }
-
-// // console.log(americanLengths);
-
-
-
-
-
-// // // Calculate from inches to cm
-// // // Store the static number of an inch 
-// // let inch:number = 2.54;
-// // let foot:number = 12;
-// // let yard:number = foot * 3;
-// // let mile:number = foot * 5.280;
-
-
-
-// // // Function to calculate inches to cm
-// // function calculateInchesToCm (inches: number) {
-// //   return inches * inch;
-// // }
-
-// // // console log to check the result
-// // console.log(calculateInchesToCm (5), "cm");
-
-
-
-// // // Calculate from cm to inches
-// // // Store the static number of a cm
-// // let cmToInch:number = 0.393701;
-
-// // // Function to calculate cm to inches
-// // function calculateCmToInches (cms: number) {
-// //   return cms * cmToInch;
-// // }
-
-// // // console log to check the result
-// // console.log(calculateCmToInches (5), "inches");
-
-
-
-
-
-
-
-
-
-
-
-// // interface Americanweights {
-// //   pound: number,
-// //   ounce: number,
-// //   ton: number,
-// // }
-
-// // const americanweights: Americanweights = {
-// //   pound: 2.54,
-// //   ounce: 12,
-// //   ton: 36,
-// // }
-
-// // console.log("American weights", americanweights);
-
-// // // Store the static number of weights 
-// // let pound:number = 2.54;
-// // let ounce:number = 12;
-// // let ton:number = foot * 3;
-
-
-
-
-// // interface Europeanweights {
-// //   gram: number,
-// //   kilogram: number,
-// //   metricTon: number,
-// // }
-
-// // const europeanweights: Europeanweights = {
-// //   gram: 2.54,
-// //   kilogram: 12,
-// //   metricTon: 36,
-// // }
-
-// // console.log("European weights", europeanweights);
-
-
-
-
-
-// // from pound to european weights
-// // Store the static number of weights from pound to european weights
-// let poundToGram:number = 453.59237;
-// let poundToKilogram:number = poundToGram / 1000;
-// let poundToMetricTon:number = poundToGram / 1000000;
-
-// // Function to calculate pound to gram
-// function calculatePoundToGram (pound: number) {
-//   return pound * poundToGram;
-// }
-
-// console.log(calculatePoundToGram (5), "grams");
-
-
-
-// // function to calculate pound to kilogram
-// function calculatePoundToKilogram (pound: number) {
-//   return pound * poundToKilogram;
-// }
-
-// console.log(calculatePoundToKilogram (5), "kilograms");
-
-
-// // function to calculate pound to metric ton
-// function calculatePoundToMetricTon (pound: number) {
-//   return pound * poundToMetricTon;
-// }
-
-// console.log(calculatePoundToMetricTon (5), "metric tons");
-
-
-
-
-// // from ounce to european weights
-// // Store the static number of weights from ounce to european weights
-// let ounceToGram:number = 28.3495;
-// let ounceToKilogram:number = ounceToGram / 1000;
-// let ounceToMetricTon:number = ounceToGram / 1000000;
-
-
-// // Function to calculate ounce to gram
-// function calculateOunceToGram (ounce: number) {
-//   return ounce * ounceToGram;
-// }
-
-// console.log(calculateOunceToGram (5), "grams");
-
-
-
-// // function to calculate ounce to kilogram
-// function calculateOunceToKilogram (ounce: number) {
-//   return ounce * ounceToKilogram;
-// }
-
-// console.log(calculateOunceToKilogram (5), "kilograms");
-
-
-// // function to calculate ounce to metric ton
-// function calculateOunceToMetricTon (ounce: number) {
-//   return ounce * ounceToMetricTon;
-// }
-
-// console.log(calculateOunceToMetricTon (5), "metric tons");
-
-
-
-
-// // from ton to european weights
-// // Store the static number of weights from ton to european weights
-// let tonToGram:number = 907184.74;
-// let tonToKilogram:number = tonToGram / 1000;
-// let tonToMetricTon:number = tonToGram / 1000000;
-
-
-// // Function to calculate ounce to gram
-// function calculateTonToGram (ton: number) {
-//   return ton * tonToGram;
-// }
-
-// console.log(calculateTonToGram (5), "grams");
-
-
-
-// // function to calculate ounce to kilogram
-// function calculateTonToKilogram (ton: number) {
-//   return ton * tonToKilogram;
-// }
-
-// console.log(calculateTonToKilogram (5), "kilograms");
-
-
-// // function to calculate ounce to metric ton
-// function calculateTonToMetricTon (ton: number) {
-//   return ton * tonToMetricTon;
-// }
-
-// console.log(calculateTonToMetricTon (5), "metric tons");
-
-
-
-
-
-
-// // from gram to american weights
-// // Store the static number of weights from gram to american weights
-// let gramToPound:number = 453.59237;
-// let gramToOunce:number = gramToPound / 16;
-// let gramToTon:number = 0.0005;
-
-
-// // Function to calculate gram to pound
-// function calculateGramToPound (gram: number) {
-//   return gram / gramToPound;
-// }
-
-// console.log(calculateGramToPound (5), "gram to pounds");
-
-
-
-// // function to calculate gram to ounce
-// function calculateGramToOunce (ton: number) {
-//   return ton / gramToOunce;
-// }
-
-// console.log(calculateGramToOunce (5), "gram to ounces");
-
-
-// // function to calculate ounce to metric ton
-// function calculateGramToTon(gram: number) {
-//   return gram * gramToTon;
-// }
-
-// console.log(calculateGramToTon (5), "gram to tons");
-
-
-
-
-
-
-
-// // from kilogram to american weights
-// // Store the static number of weights from kilogram to american weights
-// let kilogramToPound:number = 453.59237;
-// let kilogramToOunce:number = kilogramToPound / 16;
-// let kilogramToTon:number = 0.0005;
-
-
-// // Function to calculate kilograms to pound
-// function calculateKilogramToPound (kilogram: number) {
-//   return kilogram / kilogramToPound;
-// }
-
-// console.log(calculateKilogramToPound (5), "kilograms to pounds");
-
-
-
-// // function to calculate kilograms to ounce
-// function calculateKilogramToOunce (kilogram: number) {
-//   return kilogram / kilogramToOunce;
-// }
-
-// console.log(calculateKilogramToOunce (5), "kilograms to ounces");
-
-
-// // function to calculate kilograms to metric ton
-// function calculateKilogramToTon(kilogram: number) {
-//   return kilogram * kilogramToTon;
-// }
-
-// console.log(calculateKilogramToTon (5), "kilograms to tons");
-
-
-
-
-
-
-
-// // from metric ton to american weights
-// // Store the static number of weights from metric ton to american weights
-// let metricTonToPound:number = 453.59237;
-// let metricTonToOunce:number = metricTonToPound / 16;
-// let metricTonToTon:number = 0.0005;
-
-
-// // Function to calculate metric ton to pound
-// function calculateMetricTonToPound (metricTon: number) {
-//   return metricTon / metricTonToPound;
-// }
-
-// console.log(calculateMetricTonToPound (5), "metric ton to pounds");
-
-
-
-// // function to calculate metric ton to ounce
-// function calculateMetricTonToOunce (metricTon: number) {
-//   return metricTon / metricTonToOunce;
-// }
-
-// console.log(calculateMetricTonToOunce (5), "metric ton to ounces");
-
-
-// // function to calculate metric ton to metric ton
-// function calculateMetricTonToTon(metricTon: number) {
-//   return metricTon * metricTonToTon;
-// }
-
-// console.log(calculateMetricTonToTon (5), "metric ton to tons");
-
-
-// const inputs = document.querySelectorAll('input');
-
-// console.log(inputs);
-
-// inputs.forEach(input => {
-//   console.log(input)
-// });
