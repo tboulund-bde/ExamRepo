116a2571f3f0f119889b7e0f3da3444a2949f261
Lærke Nielsen <laenie01@easv365.dk>
2023-11-01 12:11:10 +0100

    started to add american lengths and calculate from inches to cm and vise versa

--- BDE_exam/src/main.ts
+++ BDE_exam/src/main.ts
@@ -22,3 +22,60 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+
+
+// Interface about american lengths
+type americanLengths = {
+  inch: number,
+  foot: number,
+}
+
+// task 6
+interface AmericanLengths {
+  inch: number,
+  foot: number,
+  yard: number,
+  mile: number,
+}
+
+const americanLengths: AmericanLengths = {
+  inch: 2.54,
+  foot: 12,
+  yard: 36,
+  mile: 63360,
+}
+
+console.log(americanLengths);
+
+
+
+// Calculate from inches to cm
+// Store the static number of an inch 
+let inch:number = 2.54;
+let foot:number = 12;
+
+// Function to calculate inches to cm
+function calculateInchesToCm (inches: number) {
+  return inches * inch;
+}
+
+// console log to check the result
+console.log(calculateInchesToCm (5));
+
+
+
+// Calculate from cm to inches
+// Store the static number of a cm
+let cmToInch:number = 0.393701;
+
+// Function to calculate cm to inches
+function calculateCmToInches (cms: number) {
+  return cms * cmToInch;
+}
+
+// console log to check the result
+console.log(calculateCmToInches (5));
+
+
