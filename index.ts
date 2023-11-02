9c2b9f2fc8c26c7e54e52a39590b84a59a284b79
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-11-02 23:55:59 +0100

    building game logic

--- index.ts
+++ index.ts
@@ -1,13 +1,95 @@
 const board = document.querySelector('.game-container') as HTMLElement;
+const button = document.querySelector('.button') as HTMLElement;
+const winMessage = document.querySelector('.winner') as HTMLElement;
+
+
+    type Turn = 'X' | 'O' | '';
+
+    let turn: Turn = 'X';
+
+
+    function listenBoard(): void { 
+        board.addEventListener('click', runGame);
+    }
 
 function main(): void {
-    makeBox();
+    createBoard();
+    listenBoard();
 }
 
-function makeBox(): void {
+function runGame(e: Event): void {
+    const boxId: string | null = (<HTMLElement>e.target).id;
+    console.log(boxId);
+    if (boxId === null) return;
+    const box: HTMLElement | null = document.querySelector(`#${boxId}`);
+    if (box === null || box.textContent !== '') return;
+    box.textContent = turn;
+    const winner: boolean = checkWinner();
+    if (!winner) switchPlayer();
+    else {
+        endGame();
+    }
+}
+
+function endGame(): void {
+    board.removeEventListener('click', runGame);
+    button.addEventListener('click', resetGame);
+    if (winMessage === null) return;
+    winMessage.textContent = turn;
+    winMessage.setAttribute('display', 'block');
+    button.style.visibility = 'visible';
+}
+
+function resetGame(): void {
+    console.log('Game reset');
+}
+
+function checkWinner(): boolean {
+    const boxes: Array<string> = getBoxes();
+    return (
+        (boxes[0] === boxes[1] && boxes[1] === boxes[2] && boxes[0] !== '') ||
+        (boxes[3] === boxes[4] && boxes[4] === boxes[5] && boxes[3] !== '') ||
+        (boxes[6] === boxes[7] && boxes[7] === boxes[8] && boxes[6] !== '') ||
+        (boxes[0] === boxes[4] && boxes[4] === boxes[8] && boxes[0] !== '') ||
+        (boxes[2] === boxes[4] && boxes[4] === boxes[6] && boxes[2] !== '') ||
+        (boxes[1] === boxes[4] && boxes[4] === boxes[7] && boxes[1] !== '') ||
+        (boxes[0] === boxes[3] && boxes[3] === boxes[6] && boxes[0] !== '') ||
+        (boxes[2] === boxes[5] && boxes[5] === boxes[8] && boxes[2] !== '')
+        );
+}
+
+function getBoxes(): Array<string> {
+    const boxesContent: Array<string> = [];
+    for (let i = 0; i <= 8; i++) {
+        const box = document.querySelector(`#box-${i}`) as HTMLElement;
+        const boxContent: string | null = box.textContent;
+        if (boxContent === null) boxesContent.push('');
+            else {
+                boxesContent.push(boxContent);
+            }
+       }
+       return boxesContent;
+    }
+
+
+function switchPlayer(): void {
+    if (turn === 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'  
+    }
+}
+
+
+function createBoard(): void {
+    for (let i = 0; i < 9; i++) makeBox(i)
+}
+
+function makeBox(i: number): void {
     const box: HTMLDivElement = document.createElement('div');
     box.className = 'box';
-    box.textContent = 'X';
+    box.id = `box-${i}`;
+    box.textContent = '';
     board.append(box);
 }
 
