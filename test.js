0364e5917f0f596918f3c44e44c76f8672c7f669
Lærke Nielsen <laenie01@easv365.dk>
2023-11-07 19:08:00 +0100

    Revert "added small adjustments like deleting .js on import links"
    This reverts commit bb5dee3bb183ae74791920e6bb9c9b6a96ddc489.

--- test.js
+++ test.js
@@ -35,13 +35,7 @@ test("Create new weight converter", async t => {
 
     // assert
     // now expect that the results value is 0.340194 kilogram(s)
-<<<<<<< HEAD
-    .expect(Selector(".resultWeight").innerText).contains("Result: 0.340194 kilogram(s)")
-=======
-    .expect(resultWeight.textContent).eql("Result: 0.340194 kilogram(s)");
-    // .expect(Selector(".resultWeight").innerText).contains("Result: 0.340194 kilogram(s)")
->>>>>>> parent of bb5dee3 (added small adjustments like deleting .js on import links)
-    
+
 });
 
 // second test
@@ -73,11 +67,5 @@ test("Create new liquid converter", async t => {
 
     // assert
     // now expect that the results value is 47.317648000000005 cl
-<<<<<<< HEAD
-    .expect(Selector(".resultLiquid").innerText).contains("Result: 47.317648000000005 cl")
-=======
-    .expect(resultLiquid.textContent).eql("Result: 47.317648000000005 cl");
-    // .expect(Selector(".resultLiquid").innerText).contains("Result: 47.317648000000005 cl")
->>>>>>> parent of bb5dee3 (added small adjustments like deleting .js on import links)
-    
+
 });
