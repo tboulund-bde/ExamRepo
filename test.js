2f02f9d77550bf50ee549774ceec8e5dca18b396
Fienw3 <annwal01@easv365.dk>
2023-11-08 11:02:24 +0100

    testcafe tests

---
+++ test.js
@@ -0,0 +1,22 @@
+import { Selector } from "testcafe"
+
+fixture`CalculatorTest`
+    .page("./index.html");
+
+test("test1", async t => {
+    await t
+.typeText("#numA", "1")
+.typeText("#numB", "2")
+.click("#mul", { timeout: 5000 })
+
+.expect(Selector("#result").innerText).eql("2");
+});
+
+test("test2", async t => {
+    await t
+.typeText("#numA", "400")
+.typeText("#numB", "600")
+.click("#add", { timeout: 5000 })
+
+.expect(Selector("#result").innerText).eql("1000");
+});
